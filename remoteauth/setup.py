a6f17e89146f94c1996a105f6386c4382407bc58
Nana Duah <gabano2005@gmail.com>
2018-12-31 12:44:01 +0000

    Update setup.py

--- remoteauth/setup.py
+++ remoteauth/setup.py
@@ -6,7 +6,7 @@ README = 'Coming Soon'
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-remoteauth1',
+    name='django-remoteauth',
     version='0.0.1-rc1',
     packages=find_packages(exclude='remoteauth'),
     include_package_data=True,
