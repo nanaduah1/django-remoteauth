b91e27cf800a8e158882d50b04f526e3edc3c6e5
Nana Duah <nanaduah@Nanas-MacBook-Pro.local>
2019-01-02 07:36:09 -0500

    Bug when checking user role

--- remoteauth/remote_auth/require.py
+++ remoteauth/remote_auth/require.py
@@ -1,8 +1,7 @@
 from . import api
 def in_any_of_the_roles(roles:[]):
-    request = api.get_request()
-
     def user_test(user):
+        request = api.get_request()
         user_profile = request.session.get('user_profile')
         if user_profile and user_profile.get('roles', None):
             roles_set = set([r.lower() for r in roles])
