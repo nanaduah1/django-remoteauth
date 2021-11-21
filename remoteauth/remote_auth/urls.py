3750d42c85ada7af6906cc6ba6a8ec9481b29ee4
Nana Duah <gabano2005@gmail.com>
2021-11-21 16:53:52 -0500

    send only path

--- remoteauth/remote_auth/urls.py
+++ remoteauth/remote_auth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .views import apify
 urlpatterns = [
-    path("<str:path>", view=apify, name='apify_view')
+    path("<str:path>/", view=apify, name='apify_view')
 ]
