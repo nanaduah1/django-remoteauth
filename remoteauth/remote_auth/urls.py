b32ceec7e184480712ee14dcf55d78fb01b34b90
Nana Duah <gabano2005@gmail.com>
2021-11-21 15:57:51 -0500

    Url to ctach all

--- remoteauth/remote_auth/urls.py
+++ remoteauth/remote_auth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .views import apify
 urlpatterns = [
-    path("<string:path>", view=apify, name='apify_view')
+    path("<str:path>", view=apify, name='apify_view')
 ]
