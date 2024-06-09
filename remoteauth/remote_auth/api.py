ccf9bb3838dd6a0ce10d08100a34a58b0b62e068
Nana Duah <gabano2005@gmail.com>
2024-06-09 20:10:39 +0000

    Fix syntax error

--- remoteauth/remote_auth/api.py
+++ remoteauth/remote_auth/api.py
@@ -361,7 +361,7 @@ def delete(path, max_retry=3):
             logger.exception("Connecting to API endpoint {} has timed out".format(url))
             return ApiResults(error_code=NETWORK_ERROR_CODE, data={'message':"Unable to connect to remote endpoint", 'error_code': 'NETWORK_TIMEOUT_ERROR'})
         except Exception as ex:
-            logger.critical(f'Unable to connect to API{ex}'))
+            logger.critical(f'Unable to connect to API{ex}')
             return ApiResults(error_code=NETWORK_ERROR_CODE, data={'message':"Unable to connect to remote endpoint", 'error_code': 'GENERAL_NETWORK_ERROR'})
     else:
         logger.critical("Unable to obtain access token for fetch request to {0}".format(url))
