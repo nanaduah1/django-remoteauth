9990e424cab3b7b8a46ac5923d24ab047c3fe2f0
Nana Duah <gabano2005@gmail.com>
2024-07-28 08:36:50 -0400

    Increment version

--- remoteauth/views.py
+++ remoteauth/views.py
@@ -1,8 +1,8 @@
 import json
 from django.http.response import HttpResponse, JsonResponse
 
-from remote_auth import api
-from remote_auth.api import ApiResults
+from remoteauth import api
+from remoteauth.api import ApiResults
 
 API_HANDLER_MAP = dict(
     get=api.fetch,
@@ -13,29 +13,24 @@ API_HANDLER_MAP = dict(
 
 
 def apify(request, path, *args, **kwargs):
-    request_method:str = request.method or ''
+    request_method: str = request.method or ""
     api_forwarding_func = API_HANDLER_MAP.get(request_method.lower())
     if not api_forwarding_func:
         return HttpResponse(status=405)
 
-    query_string = request.META.get('QUERY_STRING','')
+    query_string = request.META.get("QUERY_STRING", "")
     files = request.FILES
     data = request.POST or request.GET
     if not data and request.body:
         data = json.loads(request.body)
-        
-    print(f'qs: {query_string}')
-    print(f'BODY: {data}')
-    print(f'FILES: {files}')
 
-    api_result:ApiResults = api_forwarding_func(
-        path=f'/{path}/?{query_string}',
-        data=data,
-        files=files
+    api_result: ApiResults = api_forwarding_func(
+        path=f"/{path}/?{query_string}", data=data, files=files
     )
 
     if api_result.ok:
         return JsonResponse(data=api_result.data)
     else:
-        return JsonResponse(status=400, data=dict(error=api_result.error_code, **api_result.data))
-     
+        return JsonResponse(
+            status=400, data=dict(error=api_result.error_code, **api_result.data)
+        )
